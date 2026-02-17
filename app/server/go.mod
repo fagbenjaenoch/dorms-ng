cef8ac205afadeec54739ffa471d46395e7131fa
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-02-17 17:07:17 +0100

    initial server scaffolding

--- app/server/go.mod
+++ app/server/go.mod
@@ -1,3 +1,8 @@
 module github.com/fagbenjaenoch/hostel-marketplace-app
 
 go 1.25.5
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/cors v1.11.1
+)
--- app/server/main.go
+++ app/server/main.go
@@ -1,7 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
 
 func main() {
-	fmt.Println("Hello from server")
+	router := mux.NewRouter()
+
+	// v1Router := router.PathPrefix("/api/v1")
+
+	router.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+		helloMessage := Response{
+			Success: true,
+			Status:  200,
+			Message: "hello from server",
+		}
+
+		ReturnJSONReponse(w, helloMessage)
+	}).Methods(http.MethodGet)
+
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMiddleware.Handler(router),
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				fmt.Printf("HTTP server error: %v\n", err)
+			}
+		}
+	}()
+
+	fmt.Println("Server started on port " + "8080")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Println("Server shutting down...")
+
+	// Create a deadline to wait for
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Doesn't block if no connections, but will otherwise wait until the timeout deadline
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server forced to shutdown: %v\n", err)
+	}
+
+	fmt.Println("Server exited properly")
+
+	http.ListenAndServe("8080", router)
+}
+
+type Response struct {
+	Success bool   `json:"success"`
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Payload any    `json:"payload"`
+}
+
+func ReturnJSONReponse(w http.ResponseWriter, response Response) {
+	responseJSON, err := json.Marshal(response)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(response.Status)
+	w.Write(responseJSON)
 }
