a5bec52671a998bd6e6f48b7c207efe61cfbdc90
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-02-19 12:53:55 +0100

    minor landing page update

--- app/web-app/app/page.tsx
+++ app/web-app/app/page.tsx
@@ -1,33 +1,24 @@
 "use client";
 
-export default function Home() {
-  const handleClick = () => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
-  };
+import { FaSearch } from "react-icons/fa";
 
+export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hostel Marketplace App
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Welcome to the Hostel Marketplace App, where you can easily find and book
-            your perfect hostel accommodation. Explore a wide range of hostels, read
-            reviews, and book your stay with just a few clicks.
-          </p>
+    <div className="min-h-screen bg-zinc-50 text-gray-900 font-sans">
+      <div className="flex justify-between px-8 py-8 bg-amber-400">
+        <div className="flex gap-4">
+          <span>🏠 Logo name</span>
+          <a href="#">Hostels near me</a>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <button
-            onClick={() => handleClick()}
-            className="flex h-12 w-fit items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
-          >
-            Fetch Data from Server
-          </button>
+        <div>Sign In</div>
+      </div>
+      <main className="flex flex-col items-center gap-10 py-32">
+        <h1 className="text-5xl font-extrabold text-center">
+          Find Hostels <br />
+          on any university campus in Nigeria 🏳️
+        </h1>
+        <div className="text-xl">
+          <FaSearch /> Start searching
         </div>
       </main>
     </div>
