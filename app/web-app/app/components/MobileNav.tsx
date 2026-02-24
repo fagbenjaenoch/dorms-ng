17d1da9a20157b1dab67f035c213e8f0d1242e90
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-02-24 20:58:38 +0100

    worked on the basic functionality of the mobile nav layover

--- app/web-app/app/components/MobileNav.tsx
+++ app/web-app/app/components/MobileNav.tsx
@@ -1,11 +1,27 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import BrandIcon from "@/app/components/ui/BrandIcon";
+import { useState } from "react";
+import { FaX } from "react-icons/fa6";
 
 export default function MobileNav() {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setNavActive((prev) => !prev);
+  };
   return (
     <div className="flex lg:hidden p-8 justify-between">
       <BrandIcon />
-      <RxHamburgerMenu size={20} className="cursor-pointer" />
+      <RxHamburgerMenu size={20} className="cursor-pointer" onClick={toggleNav} />
+
+      {navActive && (
+        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between w-full">
+            <BrandIcon />
+            <FaX className="cursor-pointer" onClick={toggleNav} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
