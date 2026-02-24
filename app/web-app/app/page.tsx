25fd4557b6b7d94de2b258cbafb820f129264881
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-02-24 18:48:52 +0100

    updated hero text

--- app/web-app/app/page.tsx
+++ app/web-app/app/page.tsx
@@ -5,7 +5,7 @@ import { FaSearch } from "react-icons/fa";
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 text-gray-900 font-sans">
-      <div className="flex justify-between px-8 py-8 bg-amber-400">
+      <div className="flex justify-between px-8 py-8">
         <div className="flex gap-4">
           <span>🏠 Logo name</span>
           <a href="#">Hostels near me</a>
@@ -15,9 +15,9 @@ export default function Home() {
       <main className="flex flex-col items-center gap-10 py-32">
         <h1 className="text-5xl font-extrabold text-center">
           Find Hostels <br />
-          on any university campus in Nigeria 🏳️
+          on any <span className="underline">university campus</span> in Nigeria
         </h1>
-        <div className="text-xl">
+        <div className="text-xl flex gap-4">
           <FaSearch /> Start searching
         </div>
       </main>
