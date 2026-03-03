826e237f82af10976f5a4eb04ceab7349b5a6ac8
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-03-03 18:57:40 +0100

    added hover for university card

--- apps/frontend/app/components/ui/UniversityCard.tsx
+++ apps/frontend/app/components/ui/UniversityCard.tsx
@@ -7,13 +7,13 @@ interface UniversityCardProps {
 
 export default function UniversityCard({ name, imageUrl }: UniversityCardProps) {
   return (
-    <div className="relative rounded-3xl overflow-hidden hover-group cursor-pointer">
+    <div className="relative rounded-3xl overflow-hidden group cursor-pointer">
       <Image
         src={imageUrl}
         alt={name}
         width={200}
         height={200}
-        className="relative hover:scale-110 transition-transform duration-300"
+        className="relative group-hover:scale-110 transition-transform duration-300"
         loading="lazy"
       />
       <div className="absolute inset-0 bg-linear-to-t from-black opacity-70 to-transparent bottom-0"></div>
