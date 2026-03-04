5f27e3c98cf5d045a10c236fea0389ff57ab8ce5
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-03-04 17:50:35 +0100

    changed landing search input placeholder

--- apps/frontend/app/components/LandingSearch.tsx
+++ apps/frontend/app/components/LandingSearch.tsx
@@ -3,7 +3,11 @@ import { FaSearch } from "react-icons/fa";
 export default function LandingSearch() {
   return (
     <div className="text-xl px-8 py-4 w-120 flex items-center gap-4 ring ring-gray-900/40 rounded-full text-gray-900/40">
-      <input type="text" className="w-full" placeholder="Start searching" />
+      <input
+        type="text"
+        className="w-full"
+        placeholder="Search for hostels, universities or locations"
+      />
       <FaSearch className="cursor-pointer relative hover:text-gray-900/60 transition ease-out" />
     </div>
   );
