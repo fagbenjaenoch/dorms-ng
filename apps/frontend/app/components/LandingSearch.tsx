8145e689b924804d06f3a54f4561f6d93eb9eaed
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-03-05 21:34:07 +0100

    updated landing search ui button

--- apps/frontend/app/components/LandingSearch.tsx
+++ apps/frontend/app/components/LandingSearch.tsx
@@ -2,13 +2,15 @@ import { FaSearch } from "react-icons/fa";
 
 export default function LandingSearch() {
   return (
-    <div className="text-xl px-8 py-4 w-120 flex items-center gap-4 ring ring-gray-900/40 rounded-full text-gray-900/40">
+    <div className="relative text-xl px-8 py-4 w-160 flex items-center gap-4 ring ring-gray-900/40 rounded-full text-gray-900/40">
       <input
         type="text"
-        className="w-full"
+        className="w-full mr-8"
         placeholder="Search for hostels, universities or locations"
       />
-      <FaSearch className="cursor-pointer relative hover:text-gray-900/60 transition ease-out" />
+      <button className="absolute top-1 right-2 group hover:bg-gray-500/20 p-4 rounded-full cursor-pointer">
+        <FaSearch className="relative group-hover:text-gray-900/60 transition ease-out" />
+      </button>
     </div>
   );
 }
