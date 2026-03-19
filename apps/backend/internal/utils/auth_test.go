0209f46b4fcd23a7a26f6590867b3d725f79cd56
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-03-19 19:02:59 +0100

    removed redundant code from util test

--- apps/backend/internal/utils/auth_test.go
+++ apps/backend/internal/utils/auth_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/fagbenjaenoch/hostel-marketplace-app/internal/utils"
-	"golang.org/x/crypto/argon2"
 )
 
 func TestPasswordHashing(t *testing.T) {
@@ -12,13 +11,6 @@ func TestPasswordHashing(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc, func(t *testing.T) {
 			hashedPassword := utils.CreatePasswordHash(tc)
-
-			argonParams, salt, h, err := utils.DecodeHash(hashedPassword)
-			if err != nil {
-				t.Fatal(err)
-			}
-			hash := argon2.IDKey([]byte(tc), []byte(salt), argonParams.Iterations, argonParams.Memory, argonParams.Parallelism, argonParams.KeyLength)
-			t.Logf("Input: %x, Hash: %x", hash, h)
 
 			if !utils.ComparePassword(tc, hashedPassword) {
 				t.Errorf("Password and hash don't match: %s", tc)
