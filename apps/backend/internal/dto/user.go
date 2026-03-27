9b1ad6d228f0d0469e00b5f8a109e4273677d1e3
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-03-27 03:24:45 +0100

    fixed missing constraint on signup

--- apps/backend/internal/dto/user.go
+++ apps/backend/internal/dto/user.go
@@ -2,13 +2,12 @@ package dto
 
 type CreateUserDto struct {
 	Email    string `json:"email" validate:"required,email"`
-	FullName string `json:"full_name" validate:"required,min=2,max=50"`
+	FullName string `json:"fullname" validate:"required,min=2,max=50"`
 	Role     string `json:"role" validate:"omitempty,oneof=admin user"`
 }
 
 type CreateUserWithPasswordDto struct {
 	CreateUserDto
-	Provider string `json:"provider" validate:"required,oneof=password"`
 	Password string `json:"password" validate:"required,min=8,max=100"`
 }
 
