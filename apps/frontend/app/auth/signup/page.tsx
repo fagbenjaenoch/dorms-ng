b6957a5c05a8962cfe4b2d2fde13be0250748f13
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-03-27 03:41:12 +0100

    fixed rebase conflicts

--- apps/frontend/app/auth/signup/page.tsx
+++ apps/frontend/app/auth/signup/page.tsx
@@ -7,16 +7,6 @@ import SignupForm from "@/components/SignupForm";
 export default function Signup() {
   return (
     <Layout>
-<<<<<<< HEAD
-      <div className="p-16 text-foreground bg-background">
-        <div>
-          <p className="text-end">
-            Already have an account?{" "}
-            <Button variant="link" className="text-base">
-              <Link href="/auth/signin">Login</Link>
-            </Button>
-          </p>
-=======
       <div className="p-16 text-foreground">
         <p className="text-end">
           Already have an account?{" "}
@@ -25,7 +15,6 @@ export default function Signup() {
           </Button>
         </p>
         <div className="max-w-2xl mx-auto">
->>>>>>> 20f069b (created signup page)
           <h1 className="mt-20 mb-8 font-sans text-3xl lg:text-4xl tracking-tighter font-extrabold">
             Find hostels with <span className="text-secondary">peace of mind.</span>
           </h1>
@@ -36,11 +25,7 @@ export default function Signup() {
           </Button>
           <div className="grid grid-cols-3 items-center text-center my-10">
             <hr className="border-gray-200" />
-<<<<<<< HEAD
-            <span className="uppercase font-bold text-xs text-gray-800">
-=======
             <span className="uppercase font-bold text-xs text-gray-600">
->>>>>>> 20f069b (created signup page)
               Or register with email
             </span>
             <hr className="border-gray-200" />
