d8bf73b12896a29f6bd2e781cb37c7e7aae9a15d
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-04-09 17:29:36 +0100

    fixed absolute postioning of search suggestions

--- apps/frontend/components/LandingSearch.tsx
+++ apps/frontend/components/LandingSearch.tsx
@@ -92,7 +92,7 @@ export default function LandingSearch() {
       </Button>
 
       {!!query.data?.payload?.length && (
-        <div className="absolute  -bottom-24 left-0 w-full flex flex-col gap-2 bg-primary-foreground shadow-lg ring-1 ring-gray-500/5 p-2 rounded-xl">
+        <div className="absolute top-full mt-3 left-0 w-full flex flex-col gap-2 bg-primary-foreground shadow-lg ring-1 ring-gray-500/5 p-2 rounded-xl">
           {query.data.payload.map((searchResult) => (
             <div
               className="cursor-pointer hover:bg-gray-500/10 p-2 rounded-md"
