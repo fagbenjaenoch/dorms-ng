ce3b11378c3142c984eb5a7e2700197a6f9fb20e
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-05-08 17:33:34 +0100

    use placeholder images for search card

--- apps/frontend/components/ui/SearchResult.tsx
+++ apps/frontend/components/ui/SearchResult.tsx
@@ -31,7 +31,9 @@ export default function SearchResultCard({
           height={200}
           className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700"
           alt={name}
-          src={null}
+          src={
+            "https://lh3.googleusercontent.com/aida/ADBb0uiGHfgr55ThFf6QFyArdeNtirsUcIUf-U_h7Q37PHOGyF5qTvy3Fglod0J6JFH-cgB77jUUx4fFEURbpcLPxM21gnuH3Up8l0I0ILqws7I8R8sax0OCvdPQks81PVVy46f6eD7-2BCksodJVJAgBYJX0sa-P49bF0NrslrcfTLEY-BesT0KlQnz2eQE0AA86RS2bCmBNfIcfAOpNT0mpqF2S35tCVdsV3Vpx1jPopqaNvccbqniv1cCSeVX23G_XFXbMGiSn6nSQKU"
+          }
         />
         <div className="absolute top-4 left-4 bg-tertiary text-on-tertiary-container px-4 py-1 rounded-full font-black text-[10px] tracking-widest shadow-lg uppercase">
           Self-Contain
--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "pub-0ef10981177a40fb9dddba7b3a460064.r2.dev",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
