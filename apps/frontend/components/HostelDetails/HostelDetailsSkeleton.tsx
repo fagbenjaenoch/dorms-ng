4e04ca0635828b33d77293b3acfa4cfe20be3776
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-05-09 09:32:05 +0100

    renamed hostel details skeleton component

--- apps/frontend/components/HostelDetails/HostelDetailsSkeleton.tsx
+++ apps/frontend/components/HostelDetails/HostelDetailsSkeleton.tsx
@@ -1,6 +1,6 @@
-export function HostelSkeleton() {
+export function HostelDetailsSkeleton() {
   return (
-    <main className="max-w-4xl mx-auto p-4 animate-pulse">
+    <main className="max-w-7xl mx-auto p-4 animate-pulse">
       <div className="h-9 bg-gray-200 dark:bg-gray-800 rounded-md w-2/3 mb-2"></div>
 
       <div className="h-5 bg-gray-200 dark:bg-gray-800 rounded-md w-1/3 mb-4"></div>
