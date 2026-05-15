497e45ee7c2588681365efcfeeb8113c7a690df6
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-05-15 22:37:31 +0100

    created place search api function

--- apps/frontend/lib/api/search.ts
+++ apps/frontend/lib/api/search.ts
@@ -1,6 +1,10 @@
-import { APIResponse, SearchResult } from "../dto";
+import { APIResponse, Place, SearchResult } from "../dto";
 
 const queryParam = "q";
+const typeParam = "type";
+const idParam = "id";
+
+type TypeEnum = "institution" | "neighborhood";
 
 export async function search(
   query: string,
@@ -8,6 +12,25 @@ export async function search(
 ): Promise<APIResponse<SearchResult[]>> {
   try {
     let path = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/search?${queryParam}=${query}`;
+
+    const res = await fetch(path, {
+      method: "GET",
+      signal,
+    });
+
+    if (!res.json) throw new Error("could not search item");
+    return res.json();
+  } catch (err) {
+    throw new Error("could not search item");
+  }
+}
+
+export async function placeSearch(
+  query: string,
+  { signal }: { signal: AbortSignal },
+): Promise<APIResponse<Place[]>> {
+  try {
+    let path = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/search/places?${queryParam}=${query}`;
 
     const res = await fetch(path, {
       method: "GET",
