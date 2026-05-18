5c9a10f1677bbad812482a0f2af9effde5c251cd
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-05-18 15:27:23 +0100

    added icon for neighborhoods

--- apps/frontend/lib/utils/search.tsx
+++ apps/frontend/lib/utils/search.tsx
@@ -1,4 +1,4 @@
-import { GraduationCapIcon } from "lucide-react";
+import { GraduationCapIcon, MapPin } from "lucide-react";
 import { MdApartment } from "react-icons/md";
 
 export const EntityTypeToIcon: Record<string, React.ReactNode> = {
@@ -12,6 +12,11 @@ export const EntityTypeToIcon: Record<string, React.ReactNode> = {
       <GraduationCapIcon className="text-primary" size={15} />
     </span>
   ),
+  neighborhood: (
+    <span className="bg-orange-200 text-orange-500  px-3 py-3 flex items-center rounded-md">
+      <MapPin size={15} />
+    </span>
+  ),
   hostel_sm: (
     <span className="bg-secondary/10 px-1 py-1 flex items-center rounded-md">
       <MdApartment className="text-secondary" size={12} />
@@ -20,6 +25,11 @@ export const EntityTypeToIcon: Record<string, React.ReactNode> = {
   institution_sm: (
     <span className="bg-primary-light px-1 py-1 flex items-center rounded-md">
       <GraduationCapIcon className="text-primary" size={12} />
+    </span>
+  ),
+  neighborhood_sm: (
+    <span className="bg-orange-200 text-orange-500  px-3 py-3 flex items-center rounded-md">
+      <MapPin size={12} />
     </span>
   ),
 };
