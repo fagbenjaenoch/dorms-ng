facd4a91f789f6362c056edaec6bd9ab5d25555f
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-05-19 12:57:52 +0100

    refactor: moved http related utils to a single file

---
+++ apps/backend/internal/utils/http.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/fagbenjaenoch/hostel-marketplace-app/internal/dto"
+)
+
+type QueryParam string
+
+func (q QueryParam) String() string {
+	return string(q)
+}
+
+const (
+	SearchParam QueryParam = "q"
+	IdParam     QueryParam = "id"
+	TypeParam   QueryParam = "type"
+)
+
+func WriteJSON(w http.ResponseWriter, response dto.StructuredResponse) {
+	responseJSON, err := json.Marshal(response)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(response.Status)
+	w.Write(responseJSON)
+}
+
+func DecodeJSONBody(w http.ResponseWriter, r *http.Request, body any) error {
+	if err := json.NewDecoder(r.Body).Decode(body); err != nil {
+		WriteJSON(w, dto.StructuredResponse{
+			Success: false,
+			Status:  http.StatusBadRequest,
+			Message: err.Error(),
+		})
+		return err
+	}
+	defer r.Body.Close()
+
+	return nil
+}
+
+func GetValidatedPayloadFromRequest[T any](ctx context.Context) (T, error) {
+	payload, ok := ctx.Value(ValidatedPayloadKey).(T)
+	if !ok {
+		return payload, errors.New("invalid payload")
+	}
+	return payload, nil
+}
--- apps/backend/internal/utils/utils.go
+++ apps/backend/internal/utils/utils.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
 	pkgError "errors"
 	"fmt"
 	"net/http"
@@ -11,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/fagbenjaenoch/hostel-marketplace-app/internal/dto"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -25,42 +21,6 @@ const (
 
 func IsProduction() bool {
 	return os.Getenv("ENV") == "production"
-}
-
-func WriteJSON(w http.ResponseWriter, response dto.StructuredResponse) {
-	responseJSON, err := json.Marshal(response)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal Server Error"))
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(response.Status)
-	w.Write(responseJSON)
-}
-
-func DecodeJSONBody(w http.ResponseWriter, r *http.Request, body any) error {
-	if err := json.NewDecoder(r.Body).Decode(body); err != nil {
-		WriteJSON(w, dto.StructuredResponse{
-			Success: false,
-			Status:  http.StatusBadRequest,
-			Message: err.Error(),
-		})
-		return err
-	}
-	defer r.Body.Close()
-
-	return nil
-}
-
-func GetValidatedPayloadFromRequest[T any](ctx context.Context) (T, error) {
-	payload, ok := ctx.Value(ValidatedPayloadKey).(T)
-	if !ok {
-		return payload, errors.New("invalid payload")
-	}
-	return payload, nil
 }
 
 type ValidationError struct {
