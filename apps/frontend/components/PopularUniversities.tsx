7fc7c8e1a74c76ff7911868d687608c9850a9021
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-05-28 18:19:22 +0100

    added popular universities link to popular universities section

--- apps/frontend/components/PopularUniversities.tsx
+++ apps/frontend/components/PopularUniversities.tsx
@@ -1,19 +1,29 @@
 import SectionHeading from "./ui/SectionHeader";
 import UniversityCard from "./ui/UniversityCard";
 
+const popularUniversities = [
+  { name: "University of Ilorin", imageUrl: "/unilorin.webp", slug: "unilorin" },
+  { name: "University of Ibadan", imageUrl: "/ui.webp", slug: "ui" },
+  { name: "University of Lagos", imageUrl: "/unilag.webp", slug: "unilag" },
+  {
+    name: "Federal University of Tehcnology, Akure",
+    imageUrl: "/futa.webp",
+    slug: "futa",
+  },
+];
 export default function PopularUniversities() {
   return (
     <section className="bg-muted-foreground/10">
       <div className="container px-32">
         <SectionHeading label="Popular Universities" description="Destinations" />
         <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
-          <UniversityCard name="University of Ilorin" imageUrl="/unilorin.webp" />
-          <UniversityCard name="University of Ibadan" imageUrl="/ui.webp" />
-          <UniversityCard name="University of Lagos" imageUrl="/unilag.webp" />
-          <UniversityCard
-            name="Federal University of Technology, Akure"
-            imageUrl="/futa.webp"
-          />
+          {popularUniversities.map((university) => (
+            <UniversityCard
+              name={university.name}
+              imageUrl={university.imageUrl}
+              slug={university.slug}
+            />
+          ))}
         </div>
       </div>
     </section>
--- apps/frontend/components/ui/UniversityCard.tsx
+++ apps/frontend/components/ui/UniversityCard.tsx
@@ -1,33 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 import { HTMLAttributes } from "react";
 
 interface UniversityCardProps {
   name: string;
   imageUrl: string;
+  slug: string;
 }
 
 export default function UniversityCard({
   name,
   imageUrl,
+  slug,
   ...props
 }: UniversityCardProps & HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      {...props}
       className="relative rounded-3xl overflow-hidden group cursor-pointer"
+      {...props}
     >
-      <Image
-        src={imageUrl}
-        alt={name}
-        width={500}
-        height={200}
-        className="relative group-hover:scale-110 transition-transform duration-300"
-        loading="lazy"
-      />
-      <div className="absolute inset-0 bg-linear-to-t from-black opacity-70 to-transparent bottom-0"></div>
-      <p className="absolute bottom-0 text-white text-xl font-bold p-4 leading-5">
-        {name}
-      </p>
+      <Link href={`/institutions/${slug}`}>
+        <Image
+          src={imageUrl}
+          alt={name}
+          width={500}
+          height={200}
+          className="relative group-hover:scale-110 transition-transform duration-300"
+          loading="lazy"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-black opacity-70 to-transparent bottom-0"></div>
+        <p className="absolute bottom-0 text-white text-xl font-bold p-4 leading-5">
+          {name}
+        </p>
+      </Link>
     </div>
   );
 }
