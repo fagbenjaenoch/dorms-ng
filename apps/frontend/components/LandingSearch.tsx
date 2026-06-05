217d07306123f324fbbfe0e2d9f04ceb9cc700f4
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-05 19:14:18 +0100

    added ui for no search result in landing search

--- apps/frontend/components/LandingSearch.tsx
+++ apps/frontend/components/LandingSearch.tsx
@@ -62,37 +62,39 @@ export default function LandingSearch() {
 
       {showDropdown && !query.data ? (
         <LandingSearchDropdown />
-      ) : (
-        !!query.data?.payload?.length && (
-          <div className="absolute z-50 top-full mt-3 left-0 w-full flex flex-col gap-2 bg-primary-foreground shadow-lg ring-1 ring-gray-500/5 p-2 rounded-xl">
-            {query.data.payload.map((searchResult) => (
-              <Link
-                className="group cursor-pointer hover:bg-gray-500/10 p-4 rounded-md flex items-center gap-2"
-                href={
-                  searchResult.entity_type === "neighborhood"
-                    ? `/search?${searchQueryParam}=${searchResult.entity}`
-                    : `/${searchResult.entity_type}s/${searchResult.slug}`
-                }
-                onClick={() => addSearch(searchTerm)}
-                key={searchResult.entity_id}
-              >
-                <div className="flex items-center gap-4">
-                  {EntityTypeToIcon[searchResult.entity_type]}
-                  <div className="flex flex-col">
-                    <span className="font-semibold">{searchResult.entity}</span>
-                    <span className="text-muted-foreground">
-                      {searchResult.address}
-                    </span>
-                  </div>
+      ) : !!query.data?.payload?.length ? (
+        <div className="absolute z-50 top-full mt-3 left-0 w-full flex flex-col gap-2 bg-primary-foreground shadow-lg ring-1 ring-gray-500/5 p-2 rounded-xl">
+          {query.data.payload.map((searchResult) => (
+            <Link
+              className="group cursor-pointer hover:bg-gray-500/10 p-4 rounded-md flex items-center gap-2"
+              href={
+                searchResult.entity_type === "neighborhood"
+                  ? `/search?${searchQueryParam}=${searchResult.entity}`
+                  : `/${searchResult.entity_type}s/${searchResult.slug}`
+              }
+              onClick={() => addSearch(searchTerm)}
+              key={searchResult.entity_id}
+            >
+              <div className="flex items-center gap-4">
+                {EntityTypeToIcon[searchResult.entity_type]}
+                <div className="flex flex-col">
+                  <span className="font-semibold">{searchResult.entity}</span>
+                  <span className="text-muted-foreground">
+                    {searchResult.address}
+                  </span>
                 </div>
-                <ChevronRight
-                  className="text-primary ml-auto group-hover:translate-x-1 transition-all"
-                  size={15}
-                />
-              </Link>
-            ))}
-          </div>
-        )
+              </div>
+              <ChevronRight
+                className="text-primary ml-auto group-hover:translate-x-1 transition-all"
+                size={15}
+              />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className="absolute z-50 top-full mt-3 left-0 w-full bg-primary-foreground shadow-lg ring-1 ring-gray-500/5 p-4 rounded-xl text-muted-foreground">
+          No matches found
+        </div>
       )}
     </div>
   );
