15fa56ee407993a7f7647f2382ce2b4ca519b5e9
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-06 19:19:19 +0100

    improved button background color on hover state

--- apps/frontend/components/ui/button.tsx
+++ apps/frontend/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline hover:text-secondary cursor-pointer",
-        neutral: "bg-white text-foreground hover:bg-gray-500",
+        neutral: "bg-white text-foreground hover:bg-gray-100",
       },
       size: {
         default:
