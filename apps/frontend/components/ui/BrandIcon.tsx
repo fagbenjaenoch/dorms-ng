ef9670a140cfbee627d7485a254fc8e86e91a5c8
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-23 16:20:22 +0100

    changed brand icon text to dorms.ng

--- apps/frontend/components/ui/BrandIcon.tsx
+++ apps/frontend/components/ui/BrandIcon.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes } from "react";
 export default function BrandIcon({ ...props }: HTMLAttributes<HTMLSpanElement>) {
   return (
     <span className="font-black tracking-tighter block" {...props}>
-      Hostel<span className="text-orange-500">.ng</span>
+      Dorms<span className="text-orange-500">.ng</span>
     </span>
   );
 }
