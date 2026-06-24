ee2fcd0d4ae544858ad15716dafbdf38bb246e0c
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-24 01:03:41 +0100

    increase gap between sections in footer on mobile

--- apps/frontend/components/ui/Footer.tsx
+++ apps/frontend/components/ui/Footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
   return (
     <footer className="bg-primary text-white pt-10 md:pt-20 pb-10">
       <div className="max-w-7xl mx-auto px-8">
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-6 md:gap-12 mb-16">
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-12 mb-16">
           <div className="md:col-span-4">
             <Link href="/">
               <BrandIcon className="text-lg md:text-2xl mb-6 font-bold" />
