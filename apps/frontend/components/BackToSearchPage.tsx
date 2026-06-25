963f0a40cd64164728c2a1b230c19f3bd6a50065
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-25 01:08:36 +0100

    change back to search page button to pop history

--- apps/frontend/components/BackToSearchPage.tsx
+++ apps/frontend/components/BackToSearchPage.tsx
@@ -6,11 +6,7 @@ export default function BackToSearchPageButton() {
   const router = useRouter();
 
   return (
-    <Button
-      variant="ghost"
-      className="flex gap-2"
-      onClick={() => router.push("/search")}
-    >
+    <Button variant="ghost" className="flex gap-2" onClick={() => router.back()}>
       <BiArrowBack />
       Back to search page
     </Button>
