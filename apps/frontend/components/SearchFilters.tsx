e1eca8ca9950096951b5ff75a0f266629801ff0c
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-26 22:45:26 +0100

    added price range slider to filter component

--- apps/frontend/components/SearchFilters.tsx
+++ apps/frontend/components/SearchFilters.tsx
@@ -12,28 +12,56 @@ import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Field, FieldLabel } from "./ui/field";
 import { Checkbox } from "./ui/checkbox";
+import { Slider } from "./ui/slider";
+import { useState } from "react";
+import useMoneyFormat from "@/lib/hooks/useMoneyFormat";
 
 export default function SearchFilters() {
+  const defaultPriceRange = [200_000, 300_000];
+  const maxPrice = 10_000_000;
+  const minPrice = 20_000;
+  const formatter = useMoneyFormat("standard");
+  const [minMax, setMinMax] = useState(defaultPriceRange);
+
   return (
     <Sheet>
-      <SheetTrigger>
-        <Button variant="ghost">
-          <LucideListFilter />
-          Filters
-        </Button>
-      </SheetTrigger>
+      <SheetTrigger
+        render={
+          <Button variant="ghost">
+            <LucideListFilter />
+            Filters
+          </Button>
+        }
+      ></SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <div className="flex gap-2 justify-between w-[65%]">
             <Button variant="ghost">Clear Filters</Button>
-            <SheetTitle className="text-xl font-bold text-center">
-              Filters
-            </SheetTitle>
+            <SheetTitle className="text-xl font-bold text-center">Filter</SheetTitle>
           </div>
         </SheetHeader>
         <div className="px-4 flex flex-col gap-4">
           <div>
-            <Label>Min Price</Label>
+            <Label className="text-lg font-bold">
+              Price Range: {minMax.join(", ")}
+            </Label>
+            <div className="relative w-full">
+              <span className="absolute left-0 -top-5 text-xs text-muted-foreground">
+                {formatter.format(minPrice)}
+              </span>
+              <span className="absolute right-0 -top-5 text-xs text-muted-foreground">
+                {formatter.format(maxPrice)}
+              </span>
+              <Slider
+                className="my-6"
+                defaultValue={defaultPriceRange}
+                value={minMax}
+                onValueChange={(value) => setMinMax(value as number[])}
+                min={minPrice}
+                max={maxPrice}
+                step={1}
+              />
+            </div>
           </div>
           <Field orientation="horizontal">
             <FieldLabel>Verfied listings</FieldLabel>
