0fc0efb820813b9724c28b279b8665a79c3221f2
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-28 02:29:41 +0100

    added icon and removed error message from hostel result error component

--- apps/frontend/components/HostelResultsError.tsx
+++ apps/frontend/components/HostelResultsError.tsx
@@ -1,5 +1,6 @@
 import { FaRedo } from "react-icons/fa";
 import { Button } from "./ui/button";
+import { BsHouseSlashFill } from "react-icons/bs";
 
 interface HostelResultsErrorProps {
   error: Error;
@@ -11,9 +12,9 @@ export default function HostelResultsError({
   resetErrorBoundary,
 }: HostelResultsErrorProps) {
   return (
-    <div className="mx-auto max-w-3xl flex flex-col items-center gap-1 p-4 text-muted-foreground">
+    <div className="mx-auto max-w-3xl flex flex-col items-center gap-1 mt-20 p-4 text-muted-foreground">
+      <BsHouseSlashFill size={30} className="mb-4" />
       <h1 className="text-lg font-bold">Could not get hostel results</h1>
-      <p>Error: {error.message}</p>
       <Button
         onClick={resetErrorBoundary}
         size="xl"
