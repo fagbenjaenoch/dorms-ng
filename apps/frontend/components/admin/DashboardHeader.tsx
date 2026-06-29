e8e0b12122ba767257f4a27c7ad255b03d3b3bba
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-29 03:21:30 +0100

    added required width and height properties to image component in admin dashboard header

--- apps/frontend/components/admin/DashboardHeader.tsx
+++ apps/frontend/components/admin/DashboardHeader.tsx
@@ -24,6 +24,8 @@ export default function DashboardHeader({ title }: DashboardHeaderProps) {
         <div className="flex items-center gap-3 pl-4 border-l">
           <Image
             alt="Administrator profile"
+            width={100}
+            height={100}
             className="w-10 h-10 rounded-full border-2 border-primary/20 object-cover transition-transform duration-300 hover:scale-110 cursor-pointer"
             src="https://lh3.googleusercontent.com/aida-public/AB6AXuCdf2h4yx7EGENFDiKWqrip5N93usWX-5RjXmKqIdI260XOBGNIYzIDcyJrHCYlWyeGJjh3aihhjhhZnm4eWim-53Jg58l1cVyK97McN6GcSIFIm0w5w-dJyvhWD4-1Y1YYxOOP-sTqW_Oqf9cuNrMtW1fo4GZ4a7GyUC6EN6QZ7actA1LSdiskiBJhYHKInNXYq1xzsQ5F9TLb6aGHmfaBcK6RMfyFNYHCKWjmuJRLzNnG0YFA_mrrR2Sjao2hNQlH38IGYIx-54oB"
             referrerPolicy="no-referrer"
