c29368eecc46c505f04d3b60ec77eae2586e8a63
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-06-29 03:41:51 +0100

    reduced footer section header size and padding

--- apps/frontend/components/ui/Footer.tsx
+++ apps/frontend/components/ui/Footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
           <div className="text-sm md:col-span-8 grid grid-cols-2 lg:grid-cols-3 gap-8">
             {Object.entries(footerData).map(([section, items]) => (
               <div key={section}>
-                <h4 className="font-bold md:font-black uppercase tracking-widest text-orange-500 mb-3 md:mb-6">
+                <h4 className="font-bold text-xs md:font-black uppercase tracking-widest text-orange-500 mb-2 md:mb-4">
                   {section}
                 </h4>
                 <ul className="space-y-2 md:space-y-4">
