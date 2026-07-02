5f92295be793481ece1f3442e031af110fac4d53
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-07-02 15:56:44 +0100

    reduced font size on mobile

--- apps/frontend/components/BackToSearchPage.tsx
+++ apps/frontend/components/BackToSearchPage.tsx
@@ -6,7 +6,11 @@ export default function BackToSearchPageButton() {
   const router = useRouter();
 
   return (
-    <Button variant="ghost" className="flex gap-2" onClick={() => router.back()}>
+    <Button
+      variant="ghost"
+      className="flex text-sm md:text-base gap-2"
+      onClick={() => router.back()}
+    >
       <BiArrowBack />
       Back to search page
     </Button>
