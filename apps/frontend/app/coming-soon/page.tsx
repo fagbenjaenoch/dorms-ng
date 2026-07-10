65c6338315f83676e18f36e3fe0b7a9f6b2a2158
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-07-10 16:13:50 +0100

    create coming soon page

---
+++ apps/frontend/app/coming-soon/page.tsx
@@ -0,0 +1,30 @@
+import DesktopNav from "@/components/DesktopNav";
+import MobileNav from "@/components/MobileNav";
+import { Button } from "@/components/ui/button";
+import Footer from "@/components/ui/Footer";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <nav>
+        <DesktopNav />
+        <MobileNav />
+      </nav>
+      <main className="grow flex flex-col items-center justify-center px-6 py-24">
+        <div className="max-w-4xl w-full flex flex-col items-center text-center">
+          <div className="relative mb-8">
+            <h1 className="text-[12rem] bg-clip-text leading-52 text-transparent bg-linear-to-b from-primary-light to-primary-light/40 to-90% md:text-[16rem] font-black text-linear select-none">
+              Coming Soon
+            </h1>
+          </div>
+          <p>
+            We are working on this feature and we don't want to spoil the suprise until
+            it's perfect
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
