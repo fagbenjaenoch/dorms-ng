7b8c6016b1f5f2069d8fadbb9040f74cfa977bfe
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-07-17 18:49:51 +0100

    made minor ui improvements to hostel results ui

--- apps/frontend/components/HostelResults.tsx
+++ apps/frontend/components/HostelResults.tsx
@@ -49,7 +49,7 @@ export default function HostelResults({ areaName, showInsight }: HostelResultPro
               within <b>{areaName}</b>
             </p>
           )}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {hostels.map(
               (
                 {
