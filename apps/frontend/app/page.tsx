9b2002f5a1b942b30639b82e1f6984a3d12f98d7
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-07-28 17:50:35 +0100

    reduce verified badge font size in hero section

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <section className="min-h-screen">
         <div className="container flex flex-col lg:flex-row gap-16 lg:gap-24 items-center">
           <div className="flex flex-col gap-6 lg:pb-28 lg:pt-10 max-w-prose">
-            <div className="flex items-center gap-1 bg-primary-light text-primary px-2 py-1 text-xs font-semibold w-fit tracking-wider rounded-full uppercase">
+            <div className="flex items-center gap-1 bg-primary-light text-primary px-2 py-1 text-[.7rem] font-semibold w-fit tracking-wider rounded-full uppercase">
               <GoVerified /> Verified Nigerian student housing
             </div>
             <h1 className="font-sans text-5xl lg:text-6xl tracking-tighter font-extrabold">
