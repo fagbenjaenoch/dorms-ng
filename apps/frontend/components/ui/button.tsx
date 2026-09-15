04723b770c974778b44b320311323e69135dc0e1
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-09-15 09:32:35 +0100

    add text variant to button element

--- apps/frontend/components/ui/button.tsx
+++ apps/frontend/components/ui/button.tsx
@@ -19,7 +19,8 @@ const buttonVariants = cva(
           "text-primary hover:text-primary/80 aria-expanded:bg-muted aria-expanded:text-background dark:hover:bg-muted/50",
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
-        link: "text-primary underline-offset-4 hover:underline hover:text-secondary cursor-pointer",
+        link: "text-primary underline-offset-4 hover:underline hover:text-secondary",
+        text: "text-primary text-base",
         neutral: "bg-white text-foreground hover:bg-gray-100",
       },
       size: {
