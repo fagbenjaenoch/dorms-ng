59097eb44350e1841ec8285f7fae103473b47505
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-09-17 18:38:25 +0100

    append 'hostel' to hostel name on hostel page

--- apps/frontend/components/HostelDetails/HostelDetailsClient.tsx
+++ apps/frontend/components/HostelDetails/HostelDetailsClient.tsx
@@ -121,7 +121,7 @@ export default function HostelDetailsClient() {
       <div className="mb-6 flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
         <div>
           <h1 className="text-3xl md:text-5xl font-extrabold tracking-tighter text-on-surface flex items-center gap-3">
-            {hostel.name}
+            {hostel.name} Hostel
           </h1>
           <p className="text-sm lg:text-base flex items-center gap-1 mt-2 font-medium text-muted-foreground">
             <MapPin className="text-primary shrink-0 hidden lg:inline-flex" size={13} />
